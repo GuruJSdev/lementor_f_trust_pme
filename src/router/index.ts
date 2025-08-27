3c03b3d23f463e5561f960c1481c780133b479ed
TONY YAVICH <yavich.tony@ayokai.com>
2025-08-27 14:32:58 +0100

    update v3

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,9 @@ import RegisterPage from '../pages/RegisterPage.vue';
 import HomePage from '../pages/HomePage.vue';
 import DashboardPage from '../pages/DashboardPage.vue';
 
-import { useAuthStore } from '@/stores/auth';
+// import { useAuthStore } from '@/stores/auth';
 
-const routes = [
+const routes:any = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginPage },
   { path: '/register', component: RegisterPage },
@@ -23,26 +23,26 @@ const routes = [
   },
 ];
 
-const router = createRouter({
+const router:any = createRouter({
   history: createWebHistory(),
   routes,
 });
 
 
-router.beforeEach(async (to, _from, next) => {
-  const auth = useAuthStore();
-  if (to.meta.requiresAuth) {
-    const token = localStorage.getItem('token');
-    if (!token) return next('/login');
-    try {
-      if (!auth.user) await auth.getAccount();
-      return next();
-    } catch {
-      auth.logout();
-      return next('/login');
-    }
-  }
-  next();
-});
+// router.beforeEach(async (to, _from, next) => {
+//   const auth = useAuthStore();
+//   if (to.meta.requiresAuth) {
+//     const token = localStorage.getItem('token');
+//     if (!token) return next('/login');
+//     try {
+//       if (!auth.user) await auth.getAccount();
+//       return next();
+//     } catch {
+//       auth.logout();
+//       return next('/login');
+//     }
+//   }
+//   next();
+// });
 
 export default router;
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,77 +1,77 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
-import { ref } from 'vue';
+// import { defineStore } from 'pinia';
+// import axios from 'axios';
+// import { ref } from 'vue';
 
-export const useAuthStore = defineStore('auth', () => {
-  const user = ref(null);
-  const token = ref(localStorage.getItem('token') || null);
-  const error = ref(null);
-  const loading = ref(false);
+// export const useAuthStore = defineStore('auth', () => {
+//   const user = ref(null);
+//   const token = ref(localStorage.getItem('token') || null);
+//   const error = ref(null);
+//   const loading = ref(false);
 
-  const setError = (msg: string) => {
-    // error.value = msg;
-    setTimeout(() => (error.value = null), 5000);
-  };
+//   const setError = (msg: string) => {
+//     // error.value = msg;
+//     setTimeout(() => (error.value = null), 5000);
+//   };
 
-const getAccount = async () => {
-  if (!token.value) throw new Error('Aucun token');
-  loading.value = true;
-  try {
-    const { data } = await axios.get('http://localhost:3000/api/auth/account', {
-      headers: { Authorization: `Bearer ${token.value}` }, // utile même avec l'interceptor
-    });
-    user.value = data; // objet utilisateur retourné par le backend
-    return data;
-  } finally {
-    loading.value = false;
-  }
-};
+// const getAccount = async () => {
+//   if (!token.value) throw new Error('Aucun token');
+//   loading.value = true;
+//   try {
+//     const { data } = await axios.get('http://localhost:3000/api/auth/account', {
+//       headers: { Authorization: `Bearer ${token.value}` }, // utile même avec l'interceptor
+//     });
+//     user.value = data; // objet utilisateur retourné par le backend
+//     return data;
+//   } finally {
+//     loading.value = false;
+//   }
+// };
 
-  const register = async (userData: any) => {
-    loading.value = true;
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/register', userData);
+//   const register = async (userData: any) => {
+//     loading.value = true;
+//     try {
+//       const response = await axios.post('http://localhost:3000/api/auth/register', userData);
 
-      // après inscription, on redirige vers login
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-      return response.data;
-    } catch (err: any) {
-      setError(err.response?.data?.message || "Erreur lors de l'inscription");
-      throw err;
-    } finally {
-      loading.value = false;
-    }
-  };
+//       // après inscription, on redirige vers login
+//       localStorage.removeItem('token');
+//       window.location.href = '/login';
+//       return response.data;
+//     } catch (err: any) {
+//       setError(err.response?.data?.message || "Erreur lors de l'inscription");
+//       throw err;
+//     } finally {
+//       loading.value = false;
+//     }
+//   };
 
-  const login = async (email: string, password: string) => {
-    loading.value = true;
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', {
-        email,
-        password,
-      });
-      token.value = response.data.access_token;
-      user.value = response.data.user;
-      localStorage.setItem('token', token.value);
+//   const login = async (email: string, password: string) => {
+//     loading.value = true;
+//     try {
+//       const response = await axios.post('http://localhost:3000/api/auth/login', {
+//         email,
+//         password,
+//       });
+//       token.value = response.data.access_token;
+//       user.value = response.data.user;
+//       localStorage.setItem('token', token.value);
 
-      // dès que tu te connectes, tu récupères aussi le compte
-      await getAccount();
+//       // dès que tu te connectes, tu récupères aussi le compte
+//       await getAccount();
 
-      return response.data;
-    } catch (err: any) {
-      setError(err.response?.data?.message || 'Email ou mot de passe incorrect');
-      throw err;
-    } finally {
-      loading.value = false;
-    }
-  };
+//       return response.data;
+//     } catch (err: any) {
+//       setError(err.response?.data?.message || 'Email ou mot de passe incorrect');
+//       throw err;
+//     } finally {
+//       loading.value = false;
+//     }
+//   };
 
-  const logout = () => {
-    token.value = null;
-    user.value = null;
-    localStorage.removeItem('token');
-  };
+//   const logout = () => {
+//     token.value = null;
+//     user.value = null;
+//     localStorage.removeItem('token');
+//   };
 
-  return { user, token, error, loading, register, login, logout, getAccount };
-});
+//   return { user, token, error, loading, register, login, logout, getAccount };
+// });
